f093416ff38a830282c37f3f6ca2dd4b924b54c0
Priyanshi <priyanshikasaudhan8429@gmail.com>
2024-03-28 00:50:02 +0530

    CHANGES

---
+++ JS/script.js
@@ -0,0 +1,33 @@
+// TOGGLE ICON NAVBAR
+let menuIcon = document.querySelector('#menu_icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () =>{
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+};
+//  SCROLL SECTION ACTIVE LINKS
+let section = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+window.onscroll = () => {
+    section.forEach(sec =>{
+        let top = window.scrollY;
+        let offset  = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top <offset+height){
+            navLinks.forEach(links =>{
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            });
+
+        };
+    });
+// STICKY NAVBAR
+let header = document.querySelector('header');
+header.classList.toggle('sticky', window.scrollY > 100);
+// REMOVE TOGGLE ICON AND NAVBAR WHEN CLICK NAVBAR LINK(SCROLL)
+menuIcon.classList.remove('bx-x');
+navbar.classList.remove('active');
+};
