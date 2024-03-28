07245a3bec1cfeae330568c99f21c4d22d4e78ce
Priyanshi <priyanshikasaudhan8429@gmail.com>
2024-03-28 23:16:06 +0530

    changes

--- JS/script.js
+++ JS/script.js
@@ -21,7 +21,7 @@ window.onscroll = () => {
                 links.classList.remove('active');
                 document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
             });
-
+            // document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
         };
     });
 // STICKY NAVBAR
@@ -31,3 +31,25 @@ header.classList.toggle('sticky', window.scrollY > 100);
 menuIcon.classList.remove('bx-x');
 navbar.classList.remove('active');
 };
+// scroll reveal
+ScrollReveal({
+    reset:true,
+    distance: '80px',
+    duration: 2000,
+    delay: 200 
+});
+ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
+ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact form', { origin: 'bottom' });
+ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left' });
+ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
+
+
+
+// typed js
+const typed = new Typed('.multiple-text', {
+    strings: ['Fronted Developer' , 'Backend Developer'],
+    typeSpeed:100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop:true
+});
